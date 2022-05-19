6c926d8f37e75a5c2d86b2bd2169e9b919935379
Rawwrrh <sebastiancamus15@gmail.com>
2022-05-19 16:50:02 -0400

    Api Generics
    Api Teacher, Student Generics

--- lms/lms/serializer.py
+++ lms/lms/serializer.py
@@ -1,10 +1,12 @@
+from dataclasses import field
 from rest_framework import serializers
 from AppUser import models
 
-class PostCustomUser(serializers.ModelSerializer):
+class CustomStudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.customUser
         fields=[
+            'id',
             'username',
             'firstname',
             'lastname',
@@ -18,4 +20,33 @@ class PostCustomUser(serializers.ModelSerializer):
             'city',
             'picture',
             'description',
+        ]
+
+class CustomTeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Teacher
+        fields=[
+            'id',
+            'username',
+            'firstname',
+            'lastname',
+            'email',
+            'birthdate',
+            'phonel',
+            'phone2',
+            'institution',
+            'department',
+            'address',
+            'city',
+            'picture',
+            'description',
+            'tittles',
+            'experience',
+            
+        ]
+class CustomParentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Parent
+        field=[
+            'id'
         ]

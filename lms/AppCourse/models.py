2f9179011f02a39d55b2debcc10c7ebfcde56ef9
IziEd-Jhon <repositorios@izied.cl>
2022-05-19 15:03:34 -0400

    TSI-62 Modelos de actividades y recursos
    Arreglan errores en otros modelos y se añades modelos de actividades

--- lms/AppCourse/models.py
+++ lms/AppCourse/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from AppActivity.models import Activity
-
 class Course(models.Model):
     fullname    = models.CharField(max_length=254, null=False, blank=False)
     shortname   = models.CharField(max_length=100, null=True, unique=True)
@@ -9,8 +7,8 @@ class Course(models.Model):
     startdate   = models.DateTimeField(null=True)
     enddate     = models.DateTimeField(null=True)
     visible     = models.BooleanField(default=True)
-    timecreated = models.DateTimeField(auto_now_add=True, editable=False)
-    timemodified= models.DateTimeField(auto_now=True)
+    timecreated = models.DateTimeField(auto_now_add=True, editable=False, null=True)
+    timemodified= models.DateTimeField(auto_now=True, null=True)
 
     COURSE_GRADE_CHOISES = (
         (0, 'Custom'),
@@ -40,14 +38,22 @@ class Course(models.Model):
         verbose_name_plural = "Cursos"
         
 class Subject(models.Model):
-    course  = models.OneToOneField(Course, null=True, blank=True, on_delete=models.CASCADE)
+    course  = models.ForeignKey(Course, null=True, blank=True, on_delete=models.CASCADE)
     title   = models.CharField(max_length=150, blank=False, default='')
     description = models.TextField(default='')
-
-    activities  = models.ManyToManyField(Activity, blank=True)
 
     class Meta:
         verbose_name = "Materia"
         verbose_name_plural="Materias"
+
+class Section(models.Model):
+    course  = models.ForeignKey(Subject, null=True, blank=True, on_delete=models.CASCADE)
+    correlative = models.SmallIntegerField(default=0, null=True, blank=True)
+    title = models.CharField(max_length=150, blank=False, default='')
+    description = models.TextField(default='')
+
+    class Meta:
+        verbose_name = "Sección"
+        verbose_name_plural="Secciones"
 
 #https://github.com/llazzaro/django-scheduler

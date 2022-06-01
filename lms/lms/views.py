27c6a28a35f845b1471a3a501a7b8209f1f4074a
Rawwrrh <sebastiancamus15@gmail.com>
2022-06-01 13:48:42 -0400

    TSI-66

--- lms/lms/views.py
+++ lms/lms/views.py
@@ -2,13 +2,20 @@ from urllib import request
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth import login, authenticate
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
 
 class Login(ObtainAuthToken):
 
-    def post(self, request,*args,**kwargs):
-        login_serializer = self.serializer_class(data = request.data, context = {'request':request})
-        if login_serializer.is_valid():
-            print(login_serializer.validated_data['user'])
-        return Response({'mensaje': 'Este es el response'}, status = status.HTTP_200_OK)
-
-        
+     def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username
+        })
